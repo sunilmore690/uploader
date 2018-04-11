5095944845b58c7fc0f51e2b8387bcb0ae035df4
Sunil More <sunilmore690@gmail.com>
2018-04-11 20:55:23 +0530

    qa configuration changed

--- config/qa.js
+++ config/qa.js
@@ -17,29 +17,28 @@ module.exports = {
       port: 22,
       username: "ec2-user",
       privateKeyPath: "/path/to/private/pem/file"
-      
     },
     shScript:"/home/opt/standalone/runXCD.sh",
     brands: [
       {
-        optId: "3",
-        name: "Kwiat",
+        optId: "2086",
+        name: "Assael",
         priority: "normal",
         ftp: {
-          host: "feeds2.optcentral.com",
-          port: 21,
-          user: "user",
-          password: "password",
-          pass: "password"
+          host: "node2331-qa-opt-standalone.jelastic.optcentral.com",
+          port: 11021,
+          user: "opt-dev",
+          password: "OptJelastic2017",
+          pass: "OptJelastic2017"
         },
         dir: {
-          upload: "/Kwiat_Catalog_Data_Feed/",
-          enqueued: "/Kwiat_Catalog_Data_Feed/enqueued/",
-          processing: "/Kwiat_Catalog_Data_Feed/processing/",
-          error: "/Kwiat_Catalog_Data_Feed/error/",
-          processed: "/Kwiat_Catalog_Data_Feed/processed/",
-          ignore:"/Kwiat_Catalog_Data_Feed/ignore/",
-          backup:"/Kwiat_Catalog_Data_Feed/backup/",
+          upload: "/home/opt/uploader_rewrite/Assael_Catalog_Feed",
+          enqueued: "/home/opt/uploader_rewrite/Assael_Catalog_Feed/enqueued/",
+          processing: "/home/opt/uploader_rewrite/Assael_Catalog_Feed/processing/",
+          error: "/home/opt/uploader_rewrite/Assael_Catalog_Feed/error/",
+          processed: "/home/opt/uploader_rewrite/Assael_Catalog_Feed/output/",
+          ignore:"/home/opt/uploader_rewrite/Assael_Catalog_Feed/ignore/",
+          backup:"/home/opt/uploader_rewrite/Assael_Catalog_Feed/backup/",
         }
       },
       
